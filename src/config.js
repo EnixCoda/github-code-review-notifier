56546297f668998a5a245c993261d86d1fd35a88
Enix <enixcoda@gmail.com>
2018-10-26 11:45:12 +0800

    feat: edit env

--- src/config.js
+++ src/config.js
@@ -1,14 +1,17 @@
 require('now-env')
-const { FIREBASE, BOT_TOKEN, VERIFICATION_TOKEN } = process.env
+const { FIREBASE, BOT_TOKEN, VERIFICATION_TOKEN, CLIENT_SECRET, SIGNING_SECRET, CLIENT_ID } = process.env
 
 if (!FIREBASE) throw Error('missing Firebase')
-if (!BOT_TOKEN) throw Error('missing bot token')
 if (!VERIFICATION_TOKEN) throw Error('missing Slack App verification token')
+if (!CLIENT_SECRET) throw Error('missing client secret')
+if (!SIGNING_SECRET) throw Error('missing signing secret')
 
 const firebaseConfig = JSON.parse(Buffer.from(FIREBASE, 'base64').toString())
 
 module.exports = {
   firebaseConfig,
-  botToken: BOT_TOKEN,
   verificationToken: VERIFICATION_TOKEN,
+  clientID: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  signingSecret: SIGNING_SECRET,
 }
