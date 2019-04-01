81d1779e88619823204e0ab7cfde1431f5edcd6e
diartyz <diartyz@gmail.com>
2019-04-01 17:03:38 +0800

    feat: add notify of review submit

--- src/github.js
+++ src/github.js
@@ -7,10 +7,12 @@ const GITHUB_EVENT_HEADER_KEY = 'X-GitHub-Event'
 const GITHUB_EVENT_TYPES = {
   PING: 'ping',
   PULL_REQUEST: 'pull_request',
+  PULL_REQUEST_REVIEW: 'pull_request_review',
 }
 
 const GITHUB_EVENT_ACTION_TYPES = {
   REVIEW_REQUESTED: 'review_requested',
+  SUBMITTED: 'submitted',
 }
 
 const getHeader = (req, key) => req.headers && (req.headers[key] || req.headers[key.toLowerCase()])
@@ -65,6 +67,44 @@ exports.handleGitHubHook = (req, data) => {
             console.log(`could not find users for`, requesterGitHubName, `and`, reviewerGitHubName)
           }
         })
+      } else {
+        return 'unresolved action'
+      }
+    case GITHUB_EVENT_TYPES.PULL_REQUEST_REVIEW:
+      switch (data.action) {
+        case GITHUB_EVENT_ACTION_TYPES.SUBMITTED:
+          const {
+            pull_request: {
+              user: { login: requesterGitHubName },
+            },
+            review: {
+              html_url: reviewUrl,
+              user: { login: reviewerGitHubName },
+            },
+          } = data
+          return Promise.all([
+            db.loadWorkspace(workspace),
+            ...[requesterGitHubName, reviewerGitHubName].map(githubName =>
+              db.loadLinks(workspace, { githubName }).then(links => (links ? links[0].slack : null))
+            ),
+          ]).then(([{ botToken }, requesterUserID, reviewerUserID]) => {
+            const text = `Hi, ${requesterGitHubName}(<@${requesterUserID}>), new code review from ${reviewerGitHubName}(<@${reviewerUserID}>):\n${reviewUrl}`
+            const linkNotify = githubName => `\n\nPS: ${gitHubName} has not been linked yet. If he/she is in this Slack workspace, please introduce this app to!`
+            if (requesterUserID && reviewerUserID) {
+              return Promise.all([
+                sendAsBot(botToken, requesterUserID, text),
+                sendAsBot(botToken, reviewerUserID, text),
+              ])
+            } else if (requesterUserID) {
+              return sendAsBot(botToken, requesterUserID, text + linkNotify(reviewerGitHubName))
+            } else if (reviewerUserID) {
+              return sendAsBot(botToken, reviewerUserID, text + linkNotify(requesterGitHubName))
+            } else {
+              return (`Could not find users for ${requesterGitHubName} and ${reviewerGitHubName}`)
+            }
+          })
+        default:
+          return 'unresolved action'
       }
     default:
       if (!type) throw Error(`no github event header provided`)
